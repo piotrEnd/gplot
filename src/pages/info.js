3f3ca9ba8f4b40871ac0d18635ef9821fd7e3cd0
piotrEnd <piotrend@gmail.com>
2019-06-20 20:03:19 +0200

    css grid for section specs

--- src/pages/info.js
+++ src/pages/info.js
@@ -15,8 +15,8 @@ const info = () => (
 		<SEO title="informacje" />
 		<main className={styles.wrapper}>
 			<Heading heading="opis" />
-			<section className={styles.specs}>
-				<p className={styles.description}>
+			<section className={styles.description}>
+				<p>
 					Działka przylega do wiaduktu przy ulicy Głogowskiej, jednej z najważniejszych
 					komunikacyjnie aort aglomeracji poznańskiej, prowadzącej z centrum miasta w kierunku
 					południowym. Tutaj ulica Głogowska stanowi również odcinek drogi wojewódzkiej nr 196.
@@ -30,28 +30,33 @@ const info = () => (
 			</section>
 			<Heading heading="specyfikacja" />
 			<section className={styles.specs}>
-				<div className={styles.keys}>
-					<p>Lokalizacja</p>
-					<p>Numer działki</p>
-					<p>Powierzchnia działki</p>
-					<p>Warunki zabudowy</p>
-					<p>Przeznaczenie</p>
-					<p>Stan prawny</p>
-					<p>
-						Cena za m<sup>2</sup>
-					</p>
-				</div>
-				<div className={styles.values}>
-					<p>Poznań, Górczyn / Kotowo</p>
-					<p>306401_1.0037.AR_05.35/17</p>
-					<p>
-						10'129 m<sup>2</sup>
-					</p>
-					<p>MPZP Rejon ulicy Mieleszyńskiej w Poznaniu</p>
-					<p>zabudowa usługowa</p>
-					<p>własność, księga wieczysta wolna od obciążeń</p>
-					<p>1273 PLN</p>
-				</div>
+				<p className={styles.keys}>Lokalizacja</p>
+				<p className={styles.values}>Poznań, Górczyn / Kotowo</p>
+
+				<p className={styles.keys}>Numer działki</p>
+				<p className={styles.values}>306401_1.0037.AR_05.35/17</p>
+
+				<p className={styles.keys}>Powierzchnia działki</p>
+				<p className={styles.values}>
+					10'129 m<sup>2</sup>
+				</p>
+
+				<p className={styles.keys}>Uzbrojenie</p>
+				<p className={styles.values}>sieć elektroenergetyczna, wodociągowa i gazowa</p>
+
+				<p className={styles.keys}>Warunki zabudowy</p>
+				<p className={styles.values}>MPZP Rejon ulicy Mieleszyńskiej w Poznaniu</p>
+
+				<p className={styles.keys}>Przeznaczenie</p>
+				<p className={styles.values}>zabudowa usługowa do 50% powierzchni działki</p>
+
+				<p className={styles.keys}>Stan prawny</p>
+				<p className={styles.values}>własność, księga wieczysta wolna od obciążeń</p>
+
+				<p className={styles.keys}>
+					Cena za m<sup>2</sup>
+				</p>
+				<p className={styles.values}>1273 PLN</p>
 			</section>
 			<Heading heading="lokalizacja" />
 			<section className={styles.map}>
